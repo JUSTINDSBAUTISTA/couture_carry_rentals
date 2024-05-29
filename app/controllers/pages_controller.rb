adc67903980a7c4dff18bb77199ea43380c51a99
JUSTIN BAUTISTA <justinbautista0921@gmail.com>
2024-05-29 15:48:29 -0400

    Updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
-  def about
-  end
+  skip_before_action :authenticate_user!, only: :home
 
-  def contact
+  def home
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,4 @@
 class Request < ApplicationRecord
+  belongs_to :bag
+  belongs_to :user
 end
