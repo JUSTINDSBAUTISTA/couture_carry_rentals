05562cd9ee8137dc9b7e9ebe488182251f87a946
victory agho <aghovictory120@gmail.com>
2024-05-30 15:01:45 -0700

    searchbar

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -4,4 +4,10 @@ class Bag < ApplicationRecord
   CONDITIONS = ['new', 'like-new', 'used']
   validates :condition, inclusion: { in: CONDITIONS }
 
+  include PgSearch::Model
+  pg_search_scope :search_by_brand,
+    against: [ :brand ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
