ab1c3381668ae708287f387b9975385a7fbead08
JUSTIN BAUTISTA <justinbautista0921@gmail.com>
2024-05-30 23:17:35 -0400

    Modal for The price per day

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,15 @@ class Request < ApplicationRecord
   belongs_to :user
 
   validates :start_date, :end_date, presence: true
+
+  before_save :calculate_total_price
+
+  attr_accessor :total_price
+
+  private
+
+  def calculate_total_price
+    rental_days = (end_date - start_date).to_i
+    self.total_price = rental_days * bag.price_per_day
+  end
 end
